2b0390a3eed7dbecf8e48d4b7dd53a8f7d1c95ce
joh <dhgus110@naver.com>
2024-10-08 18:49:02 +0900

    [NEW]게임 씬 스크립트 뼈대

---
+++ YoLo_Tetris/Assets/Scripts/Scenes/GameScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected override void Init()
+    {
+        //base.Init();
+        SceneType = Define.Scene.Game;
+    }
+
+    public override void Clear()
+    {
+
+    }
+}
