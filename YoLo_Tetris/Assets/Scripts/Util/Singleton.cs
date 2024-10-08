fc93e481ed7394e39b3bece79c79b4cbc49bbbc7
joh <dhgus110@naver.com>
2024-10-08 18:51:07 +0900

    [NEW] 싱글톤 스크립트

---
+++ YoLo_Tetris/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton <T> : MonoBehaviour where T : MonoBehaviour
+{
+    [SerializeField] private bool dontDestroyOnLoad;
+
+    protected static T sInstance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (sInstance == null)
+            {
+                var obj = new GameObject("@" + typeof(T).ToString());
+                sInstance = obj.AddComponent<T>();
+            }
+            return sInstance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if(sInstance == null)
+        {
+            if (dontDestroyOnLoad)
+                DontDestroyOnLoad(this);
+            sInstance = this.GetComponent<T>();
+            Init();
+        }
+        else if(sInstance != this)
+        {
+            Debug.Log("Destroy @" + gameObject.name);
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected abstract void Init();
+
+    protected virtual void OnDestroy()
+    {
+        sInstance = null;
+    }
+
+}
