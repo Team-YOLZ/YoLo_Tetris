4156cce2e3d95c1ee4a9a13ba78aac28891ae3fa
joh <dhgus110@naver.com>
2024-10-16 21:47:28 +0900

    [UPDATE] 테트로미노 회전 추가

--- YoLo_Tetris/Assets/Scripts/Controller/MinoController.cs
+++ YoLo_Tetris/Assets/Scripts/Controller/MinoController.cs
@@ -7,40 +7,48 @@ using System;
 
 public class MinoController : MonoBehaviour
 {
-    [SerializeField] private RectTransform _mino; // 자기 자신 위치 
-    public float _fallingSpeed = 500.0f; //떨어지는 속도
+    [SerializeField] private RectTransform _mino; // 자기 자신 위치
+    [SerializeField] private RectTransform _gameMainCanvas;
 
-    //private bool _isMoving = false; 
+    private Vector2 curDragPosition = Vector2.zero;
+    private Vector2 PrevDragPosition = Vector2.zero;
+    private InputManager _inputManager;
+
+    private float _fallingSpeed = 300.0f; //떨어지는 속도
+    private float _quickDownForce = 8.0f; //테트로미노가 바로 떨어지게하는 힘의 양
+    private float _minoPixelSize = 70.0f; //테트로미노 70x70
+
     private bool _isLanding = false; //착지했는가?
 
-    private InputManager _inputManager;
+    readonly private Vector3 _rotateVec = new Vector3(0, 0, 90);
 
 
     private MoveDir _move = MoveDir.Idle;
-    public MoveDir MinoMove
-    {
-        get { return _move; }
-        set
-        {
-            if (_move == value)
-                return;
+    //public MoveDir MinoMove
+    //{
+    //    get { return _move; }
+    //    set
+    //    {
+    //        if (_move == value)
+    //            return;
 
-            _move = value;
-        }
-    }
+    //        _move = value;
+    //    }
+    //}
 
     private RotationDir _rotation = RotationDir.Idle;
-    public RotationDir MinoRotation
-    {
-        get { return _rotation; }
-        set
-        {
-            if (_rotation == value)
-                return;
+    //public RotationDir MinoRotation
+    //{
+    //    get { return _rotation; }
+    //    set
+    //    {
+    //        if (_rotation == value)
+    //            return;
 
-            _rotation = value;
-        }
-    }
+    //        _rotation = value;
+    //    }
+    //}
+
 
     void Awake()
     {
@@ -49,60 +57,93 @@ public class MinoController : MonoBehaviour
             _mino = gameObject.GetComponent<RectTransform>();
         }
 
+        if(_gameMainCanvas == null)
+        {
+            _gameMainCanvas = gameObject.transform.root.GetComponent<RectTransform>();
+        }
+
         if (_inputManager == null)
         {
             _inputManager = GameObject.Find("InputScreen").GetComponent<InputManager>();
         }
+
         _inputManager.TouchAction += TouchState;
+        _inputManager.IsEndDragAction += IsEndDrag;
+        _inputManager.BeginDragAction += GetBeginDragPosition;
 
         StartCoroutine(AutoDownMove());
 
     }
 
     // 화면 : 드래그 or 클릭
-    void TouchState(TouchEvent touchEvent, Vector2 position)
+    void TouchState(TouchEvent touchEvent, Vector2 position, Vector2 delta)
     {
         switch (touchEvent)
         {
             case TouchEvent.Click:
-                if (Camera.main.ScreenToWorldPoint(position).x >= 0)
-                    _rotation = RotationDir.Right;
-                else
-                    _rotation = RotationDir.Left;
+                GetRotationDir(position);
                 UpdateRotation();
                 break;
 
             case TouchEvent.Drag:
+                GetMoveDir(delta);
                 UpdateMove(position);
                 break;
         }
     }
 
-    //화면 클릭 -> 회전
+
+    #region Click Method
+    // 클릭이벤트 -> 회전
     void UpdateRotation()
     {
-        switch (MinoRotation)
+        switch (_rotation)
         {
             case RotationDir.Left:
-                //todo
+                _mino.GetComponent<Transform>().DORotate(_mino.rotation.eulerAngles - _rotateVec, 0);
                 break;
             case RotationDir.Right:
-                //todo
+                _mino.GetComponent<Transform>().DORotate(_mino.rotation.eulerAngles + _rotateVec, 0);
                 break;
         }
 
     }
 
-    //화면 드래그 -> 이동
+    void GetRotationDir(Vector2 position)
+    {
+        if (Camera.main.ScreenToWorldPoint(position).x >= 0)
+            _rotation = RotationDir.Right;
+        else
+            _rotation = RotationDir.Left;
+    }
+    #endregion
+
+
+    #region Drag Method
+    // 드래그이벤트 -> 이동
     void UpdateMove(Vector2 position)
     {
-        switch (MinoMove)
+        curDragPosition = ConvertScreentoLocalInRect(position);
+
+        switch (_move)
         {
-            case MoveDir.Left:
+            case MoveDir.HorizonDir:
                 //todo
-                break;
-            case MoveDir.Right:
-                //todo
+                float distance = PrevDragPosition.x - curDragPosition.x;
+                int force = (int)(Mathf.Round(distance) / _minoPixelSize);
+                //left
+                if (distance > 0 && Mathf.Round(distance) >= _minoPixelSize)
+                {
+                    float destPosY = _mino.anchoredPosition.y - _minoPixelSize * force;
+                    _mino.DOAnchorPosX(destPosY, 0.01f);
+                }
+                //right
+                else if (distance < 0 && Mathf.Round(distance) >= _minoPixelSize)
+                {
+                    float destPosY = _mino.anchoredPosition.y + _minoPixelSize * force;
+                    _mino.DOAnchorPosX(destPosY, 0.01f);
+                }
+
                 break;
             case MoveDir.SlowDown:
                 //todo
@@ -113,18 +154,62 @@ public class MinoController : MonoBehaviour
         }
     }
 
+    //드래그를 위한 포지션변수 초기화
+    void GetBeginDragPosition(Vector2 position)
+    {
+        curDragPosition = ConvertScreentoLocalInRect(position);
+        PrevDragPosition = curDragPosition;
+    }
 
+    //드래그중일 때 상하, 좌우 중에 어떤걸로 움직이는지
+    void GetMoveDir(Vector2 delta)
+    {
+        if(_move == MoveDir.Idle)
+        {
+            if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                _move = MoveDir.HorizonDir;
+            else if (Mathf.Abs(delta.x) < Mathf.Abs(delta.y))
+                _move = MoveDir.VerticalDir;
+            else
+                _move = MoveDir.Idle;
+        }
+
+        if(_move == MoveDir.VerticalDir)
+        {
+            if (Mathf.Abs(delta.y) > _quickDownForce)
+                _move = MoveDir.QuickDown;
+        }
+
+    }
+
+    //좌우드래그(or 수직낙하드래그)가 끝나지 않았는데 수직낙하(or 좌우드래그) 방지
+    void IsEndDrag(bool drag)
+    {
+        _move = MoveDir.Idle;
+    }
+    #endregion
+
+    //화면좌표 -> 캔버스 좌표
+    Vector2 ConvertScreentoLocalInRect(Vector2 position)
+    {
+        Vector2 canvasVec = Vector2.zero;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_gameMainCanvas, position, Camera.main, out canvasVec);
+        return canvasVec;
+    }
+
+
+    #region Coroutine Method
     IEnumerator AutoDownMove()
     {
         while (!_isLanding)
         {
-            float destPosY = _mino.anchoredPosition.y - 60f; //아래로 한 칸씩 이동
+            float destPosY = _mino.anchoredPosition.y - _minoPixelSize; //아래로 한 칸씩 이동
             _mino.DOAnchorPosY(destPosY, 0);
             yield return new WaitForSeconds(_fallingSpeed * Time.deltaTime);
         }
         StopCoroutine(AutoDownMove());
     }
-
+    #endregion
 
 
 }
