6ee0c291d75e0a9880d021c7406006a172358e69
joh <dhgus110@naver.com>
2024-10-17 18:19:52 +0900

    [NEW] BoundaryData 스크립트, 템플릿 제작

---
+++ YoLo_Tetris/Assets/Scripts/Util/TetrominoBoundaryData.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+[CreateAssetMenu]
+public class TetrominoBoundaryData : ScriptableObject
+{
+    public MinoType MinoType;
+    public BoundaryData HorizonBoundary;
+
+    [Serializable]
+    public struct BoundaryData
+    {
+        public Vector2 R0;
+        public Vector2 R1;
+        public Vector2 R2;
+        public Vector2 R3;
+    }
+
+}
