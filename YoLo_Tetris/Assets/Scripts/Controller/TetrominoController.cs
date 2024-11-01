d7e49f3b93441d0523ee9145c538adadaf74f711
joh <dhgus110@naver.com>
2024-11-01 18:45:19 +0900

    [UPDATE] slowdown 로직

--- YoLo_Tetris/Assets/Scripts/Controller/TetrominoController.cs
+++ YoLo_Tetris/Assets/Scripts/Controller/TetrominoController.cs
@@ -10,11 +10,12 @@ public class TetrominoController : MonoBehaviour
     [SerializeField] private RectTransform _tetromino; // 자기 자신 위치
     [SerializeField] private RectTransform _gameMainCanvas;
     [SerializeField] private TetrominoBoundaryData _data;
-    [SerializeField] private RectTransform[] _minos;
+    [SerializeField] private MinoController[] _minoControllers;
 
-    private const float _fallingSpeed = 1000.0f; //떨어지는 속도
-    private const float _quickDownForce = 6.5f; //테트로미노가 바로 떨어지게하는 힘의 양
-    private const float _minoPixelSize = 70.0f; //테트로미노 70x70
+    private const float _autoDownSpeed = 400.0f; //떨어지는 속도
+    private const float _slowDownSpeed = 80.0f; //떨어지는 속도
+    private const float _quickDownForce = 9.5f; //테트로미노가 바로 떨어지게하는 힘의 양
+    private const float _minoPixelSize = 65.0f; //테트로미노 65x65
     private bool _isLanding = false; //착지했는가?
 
     readonly private Vector3 _rotateVec = new Vector3(0, 0, 90);
@@ -22,10 +23,11 @@ public class TetrominoController : MonoBehaviour
     private Vector2 _PrevDragPosition = Vector2.zero;
 
     private Coroutine _autoDownCoroutine;
+    private Coroutine _slowDownCoroutine;
 
-    private MoveDir _move = MoveDir.Idle;
-    private RotationDir _rotation = RotationDir.Idle; 
-    private HorizonDir _horizonDir = HorizonDir.Idle; //드래그 왼/오
+    private MoveDir _moveDir = MoveDir.Idle;
+    private ClickHorizonDir _clickHorizonDir = ClickHorizonDir.Idle; 
+    private DragHorizonDir _dragHorizonDir = DragHorizonDir.Idle; //드래그 왼/오
     private MinoType _minoType = MinoType.Unknown;
     private MinoRotationState _minoRotationState = MinoRotationState.R0;
     public MinoRotationState MinoRotationState
@@ -77,22 +79,48 @@ public class TetrominoController : MonoBehaviour
         }
     }
 
+    void StartSlowDown()
+    {
+        StopSlowDown();
+        _slowDownCoroutine = StartCoroutine(nameof(SlowDownMove));
+    }
+
+    void StopSlowDown()
+    {
+        if(_slowDownCoroutine != null)
+        {
+            StopCoroutine(_slowDownCoroutine);
+            _slowDownCoroutine = null;
+        }
+    }
+
+    //아래로 한 칸씩 이동
+    void OneStepDownMove()
+    {
+        float destPosY = _tetromino.anchoredPosition.y - _minoPixelSize;
+        _tetromino.DOAnchorPosY(destPosY, 0);
+        //foreach(var mino in _minoControllers)
+        //{
+        //    mino.MinoMove(0, -1);
+        //}
+    }
+
 
     // 입력이벤트
-    void TouchState(TouchEvent touchEvent, RotationDir leftRight, Vector2 position, Vector2 delta)
+    void TouchState(TouchEvent touchEvent, ClickHorizonDir leftRight, Vector2 position, Vector2 delta)
     {
         switch (touchEvent)
         {
             case TouchEvent.Click:
-                _rotation = leftRight;
+                _clickHorizonDir = leftRight;
                 _minoRotationState = GetRotationState();
                 UpdateRotate();
                 UpdatePositionAfterClick();
                 break;
 
             case TouchEvent.Drag:
-                _move = GetMoveDir(delta);
-                UpdateMove(position);
+                _moveDir = GetMoveDir(delta);
+                UpdateMove(position, delta);
                 break;
         }
     }
@@ -101,13 +129,13 @@ public class TetrominoController : MonoBehaviour
     MinoRotationState GetRotationState()
     {
         var state = _minoRotationState;
-        switch (_rotation)
+        switch (_clickHorizonDir)
         {
-            case RotationDir.Left:
+            case ClickHorizonDir.Left:
                 int leftRotation = (int)_minoRotationState + 1 == 4 ? 0 : (int)_minoRotationState + 1;
                 state = (MinoRotationState)leftRotation;
                 break;
-            case RotationDir.Right:
+            case ClickHorizonDir.Right:
                 int rightRotation = (int)_minoRotationState - 1 == -1 ? 3 : (int)_minoRotationState - 1;
                 state = (MinoRotationState)rightRotation;
                 break;
@@ -118,12 +146,12 @@ public class TetrominoController : MonoBehaviour
     //회전
     void UpdateRotate()
     {
-        switch (_rotation)
+        switch (_clickHorizonDir)
         {
-            case RotationDir.Left:
+            case ClickHorizonDir.Left:
                 _tetromino.transform.DORotate(_tetromino.rotation.eulerAngles + _rotateVec, 0);
                 break;
-            case RotationDir.Right:
+            case ClickHorizonDir.Right:
                 _tetromino.transform.DORotate(_tetromino.rotation.eulerAngles - _rotateVec, 0);
                 break;
         }
@@ -164,12 +192,12 @@ public class TetrominoController : MonoBehaviour
 
     #region Drag Method
     //이동
-    void UpdateMove(Vector2 position)
+    void UpdateMove(Vector2 position, Vector2 delta)
     {
         _curDragPosition = UtilManager.ConvertScreentoLocalInRect(_gameMainCanvas,position);
         float distance = _PrevDragPosition.x - _curDragPosition.x;
 
-        switch (_move)
+        switch (_moveDir)
         {
             case MoveDir.HorizonDir:
                 int force = (int)(Mathf.Round(Mathf.Abs(distance)) / _minoPixelSize);
@@ -179,7 +207,7 @@ public class TetrominoController : MonoBehaviour
                     float destPos = _tetromino.anchoredPosition.x - _minoPixelSize * force;
                     _tetromino.DOAnchorPosX(GetClampPositionHorizonBoundary(destPos), 0);
                     _PrevDragPosition = _curDragPosition;
-                    _horizonDir = HorizonDir.Left;
+                    _dragHorizonDir = DragHorizonDir.Left;
                 }
                 //right
                 else if (distance < 0 && force >= 1)
@@ -187,12 +215,12 @@ public class TetrominoController : MonoBehaviour
                     float destPos = _tetromino.anchoredPosition.x + _minoPixelSize * force;
                     _tetromino.DOAnchorPosX(GetClampPositionHorizonBoundary(destPos), 0);
                     _PrevDragPosition = _curDragPosition;
-                    _horizonDir = HorizonDir.Right;
+                    _dragHorizonDir = DragHorizonDir.Right;
                 }
                 break;
 
             case MoveDir.SlowDown:
-                //todo
+                StartSlowDown();
                 break;
 
             case MoveDir.QuickDown:
@@ -211,25 +239,28 @@ public class TetrominoController : MonoBehaviour
     //드래그중일 때 상하, 좌우 중에 어떤걸로 움직이는지
     MoveDir GetMoveDir(Vector2 delta)
     {
-        var dir = _move;
-        if(_move == MoveDir.Idle)
+        var dir = _moveDir;
+        if(_moveDir == MoveDir.Idle)
         {
             dir = Mathf.Abs(delta.x) > Mathf.Abs(delta.y) ? MoveDir.HorizonDir :
                 Mathf.Abs(delta.x) < Mathf.Abs(delta.y) ? MoveDir.VerticalDir : dir;
         }
 
-        if (_move == MoveDir.VerticalDir)
+        if (_moveDir == MoveDir.VerticalDir)
         {
-            dir = (Mathf.Abs(delta.y) > _quickDownForce) ? MoveDir.QuickDown : dir;
+            dir = (Mathf.Abs(delta.y) > _quickDownForce) ? MoveDir.QuickDown : MoveDir.SlowDown;
         }
 
         return dir;
     }
 
-    //좌우드래그(or 수직낙하드래그)가 끝나지 않았는데 수직낙하(or 좌우드래그) 방지
     void IsEndDrag(bool drag)
     {
-        _move = MoveDir.Idle;
+        if (_moveDir == MoveDir.SlowDown)
+            StopSlowDown();
+
+        _moveDir = MoveDir.Idle;
+        
     }
 
     //이동중에 경계 못 벗어나게 좌표 구하기
@@ -260,13 +291,20 @@ public class TetrominoController : MonoBehaviour
     {
         while (!_isLanding)
         {
-            yield return new WaitForSeconds(_fallingSpeed * Time.deltaTime);
-
-            //아래로 한 칸씩 이동
-            float destPosY = _tetromino.anchoredPosition.y - _minoPixelSize;
-            _tetromino.DOAnchorPosY(destPosY, 0);
+            yield return new WaitForSeconds(_autoDownSpeed * Time.deltaTime);
+            OneStepDownMove();
         }
         StopCoroutine(AutoDownMove());
+    }
+
+    IEnumerator SlowDownMove()
+    {
+        while (!_isLanding)
+        {
+            yield return new WaitForSeconds(_slowDownSpeed * Time.deltaTime);
+            OneStepDownMove();
+        }
+        StopCoroutine(SlowDownMove());
     }
     #endregion
 
