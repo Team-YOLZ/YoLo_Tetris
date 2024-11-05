d887e289939b89a2482a660fe8ba57e76972814f
joh <dhgus110@naver.com>
2024-11-05 21:07:07 +0900

    [UPDATE] 테트로미노 생성

--- YoLo_Tetris/Assets/Scripts/Controller/BoardController.cs
+++ YoLo_Tetris/Assets/Scripts/Controller/BoardController.cs
@@ -1,28 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BoardController : MonoBehaviour
 {
-    private Dictionary<Vector2,GameObject> _minoDic;
+    [SerializeField] private Transform _TetrominosParent;
 
-    public float LandSurvey(float x)
+    private Queue<string> _spawnOrder = new Queue<string>();
+    private List<Cell> _minos = new();
+
+    private struct Cell
     {
-        int roofY = 0;
-        foreach (Vector2 key in _minoDic.Keys)
-        {
-            if(key.x == x)
-            {
-                roofY = (int)(roofY < key.y ? key.y : roofY);
-            }
-        }
-
-        return roofY;
+        int id;
+        GameObject mino;
     }
 
-    public void MinoAdd(Vector2 position, GameObject go)
+    private void Start()
     {
 
+    }
+
+    public int FindLand(int id) // or bool IsLanding()
+    {
+
+        return 0;
+    }
+
+    private void DicAdd(Vector2 position, GameObject go)
+    {
+
+    }
+
+    private void GetSpawnOrderSevenBag()
+    {
+        char[] bag = "IJLOSTZ".ToCharArray();
+        for (int i = 0; i < 7; i++)
+        {
+            int random = Random.Range(0, bag.Length);
+            _spawnOrder.Enqueue(bag[random].ToString());
+            bag = bag.Where(str => str != bag[random]).ToArray();
+        }
+
+    }
+
+    //풀링으로 바꿀 예정
+    private void TetrominoSpawn() 
+    {
+        if(_spawnOrder.Count <= 3)
+        {
+            GetSpawnOrderSevenBag();
+        }
+        string path = _spawnOrder.Peek() + "_Tetromino";
+        _spawnOrder.Dequeue();
+
+        Managers.Resource.Instantiate(path, _TetrominosParent);
     }
 
     private void MinoRemove()
